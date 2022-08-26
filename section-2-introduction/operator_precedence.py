31ec4e9b94a39f95a44703ca41a5f92042c6ea94
charlytron <ctompkins32@gmail.com>
2022-08-26 11:42:31 -0500

    Operator precedence

---
+++ section-2-introduction/operator_precedence.py
@@ -0,0 +1,18 @@
+# Operator precedence
+
+print(20 + 3 * 4)
+
+# Multiply first, then add
+
+print(20 - 3 * 4)
+
+# First, we follow the brackets
+# Second, we do the power of
+
+print((20 - 3) + 2 ** 2)
+
+# After the power of, we do the multiplication and division
+
+# **
+## * /
+### + -
