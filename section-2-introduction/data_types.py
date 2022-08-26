2b3ef3b58bc996e6df260b3fdd4233926343453f
charlytron <ctompkins32@gmail.com>
2022-08-26 08:22:24 -0500

    data types

---
+++ section-2-introduction/data_types.py
@@ -0,0 +1,31 @@
+##########################
+# Fundamental Data Types #
+##########################
+
+# Python has a set of fundamental data types that can be used to store and manipulate data.
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#############################
+#  Classes -> custom types  #
+#############################
+
+
+#############################
+# Specialized Data Types   #
+#############################
+
+# Python has a set of specialized data types from modules
+# that can be used to store and manipulate data.
+
+########################################
+None # represents the absence of a value
+########################################
+
+# Like Zero in mathematics
