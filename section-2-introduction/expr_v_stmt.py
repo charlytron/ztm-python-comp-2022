1d9870991067a8de83d16c5fa5bee23adbb2dbe2
charlytron <ctompkins32@gmail.com>
2022-08-27 07:17:13 -0500

    expression vs statement

---
+++ section-2-introduction/expr_v_stmt.py
@@ -0,0 +1,5 @@
+iq = 100
+user_age = iq / 5 # expression produces a value
+
+# statement is an entire line of code that performs an action
+# Here, we're calculating 100 / 5 and assigning the result to user_age
