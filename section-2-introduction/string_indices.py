5861d63f341c04d03a0465d991efe71f50760cb7
charlytron <ctompkins32@gmail.com>
2022-08-27 09:31:56 -0500

    string indices

---
+++ section-2-introduction/string_indices.py
@@ -0,0 +1,16 @@
+############################################################
+#                String Indices (indexes)                  #
+############################################################
+
+# String indices are the positions of the characters in a string.
+
+wolfe = 'mau mau and the flak catchers'
+       # 0123456...
+
+# Each character will be stored in memory at a certain position.
+# including the space character, there are 26 characters in the string.
+# The computer will search within the shelf from 0 to 25.
+
+print(wolfe[0]) # m
+print(wolfe[23]) # t
+
