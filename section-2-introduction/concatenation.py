866606b39b95eeeda21e44605fb6bd83361fd704
charlytron <ctompkins32@gmail.com>
2022-08-27 07:56:51 -0500

    concatenation

---
+++ section-2-introduction/concatenation.py
@@ -0,0 +1,14 @@
+############################################################
+#                    Concatenation                         #
+############################################################
+
+# Concatenation is the process of combining two or more strings together.
+
+'hello' + ' Dolly'  # => 'hello Dolly'
+'hello' + ' ' + 'Dolly'  # => 'hello Dolly'
+
+# You can also use the + operator with numbers.
+
+print('Moron' + 5) # => 'hello5' doesn't result here.
+# can't concatenate a string and a number.
+
