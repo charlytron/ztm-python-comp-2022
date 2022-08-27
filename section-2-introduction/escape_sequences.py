482f7a6c80b6edf92313b1bf5f13e410df95ce16
charlytron <ctompkins32@gmail.com>
2022-08-27 08:37:59 -0500

    escape sequences

---
+++ section-2-introduction/escape_sequences.py
@@ -0,0 +1,14 @@
+############################################################
+#                   Escape Sequences                       #
+############################################################
+
+# weather = 'It\'s rainy'
+# weather = "It's rainy"
+# weather = "It\'s \"kinda\" rainy"
+# weather = "\tIt\'s \"kinda\" rainy" # tab
+# weather = "\nIt\'s \"kinda\" rainy" # newline
+weather = "\t It\'s \"kinda\" rainy in the Windy City \n hope youse got an umbrella" # newline
+
+
+print(weather)
+
