2f1ce918dc8763de0b2d1fed1160cc123130bc58
charlytron <ctompkins32@gmail.com>
2022-08-27 07:49:41 -0500

    strings explained

---
+++ section-2-introduction/strings.py
@@ -0,0 +1,29 @@
+############################################################
+#                  STRINGS                                 #
+############################################################
+
+# Strings are sequences of characters, written with quotes.
+
+print(type('This is a string and, well, I81B4U')) # <class 'str'>
+print(type("This is also a string.")) # <class 'str'>
+
+username = "John"
+password = "segredo"
+
+long_string = '''
+This is a very long string.
+It contains newlines.
+'''
+# allows for newlines
+
+print (long_string)
+
+# Strings can be concatenated with the + operator
+
+first_name = "Panayotes"
+last_name = "Panayotides"
+full_name = first_name + " " + last_name
+
+print(full_name) # Panayotis Panayotides
+
+
