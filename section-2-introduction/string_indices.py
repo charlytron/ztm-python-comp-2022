b222e5c9a44831a34202aa2d753d96f20bb3a8e0
charlytron <ctompkins32@gmail.com>
2022-08-28 07:08:52 -0500

    string indices complete

--- section-2-introduction/string_indices.py
+++ section-2-introduction/string_indices.py
@@ -13,4 +13,20 @@ wolfe = 'mau mau and the flak catchers'
 
 print(wolfe[0]) # m
 print(wolfe[23]) # t
+print(wolfe[7]) # space
 
+# [start:end:step]
+# [0:3] will print the first three characters.
+
+sequence = '01234567'
+print(sequence[0:3]) # 012
+print(sequence[0:3:2]) # 02 step by 2
+print(sequence[1:]) # 1234567
+print(sequence[:]) # 01234567
+print(sequence[:5]) # 01234
+print(sequence[::1]) # 01234567
+print(sequence[::2]) # 0246
+print(sequence[-1]) # 7
+print(sequence[-3]) # 5
+print(sequence[::-1]) # counts backwards from the end of the string
+print(sequence[::-2]) # counts backwards by 2s from the end of the string
