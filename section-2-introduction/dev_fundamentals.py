d9d6ed83e3c7ad2fc72b32bbebfc27f411dd0af0
charlytron <ctompkins32@gmail.com>
2022-08-28 09:15:31 -0500

    development fundamentals

---
+++ section-2-introduction/dev_fundamentals.py
@@ -0,0 +1,16 @@
+###############################################################################
+#                         DEVELOPMENT FUNDAMENTALS                            #
+###############################################################################
+
+# Comments tell the interpreter to ignore the following code.
+## There are good and bad comments.
+
+### Good comments are used to explain the code.
+#### They're used when something important is said.
+##### Be concise
+###### https://realpython.com/python-comments-guide/
+
+### Bad comments add clutter to the code.
+
+# assign a variable to a value (what?)
+name = 'Murgatroyd' # (code should be self-explanatory)
