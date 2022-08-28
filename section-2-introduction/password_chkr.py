5dce24ffd0063fa04af310e551f75c7afb59c100
charlytron <ctompkins32@gmail.com>
2022-08-28 09:36:08 -0500

    password checker correction

--- section-2-introduction/password_chkr.py
+++ section-2-introduction/password_chkr.py
@@ -2,9 +2,10 @@
 #                   Password Checker                       #
 ############################################################                
 
+username = input("What is your Username? ")
 password = input("What is your password? ")
 password_length = len(password)
 password = password_length * '*'
 
 
-print(f'password {password} is {password_length} characters long')
+print(f'Hey, {username}, youse password {password} is {password_length} characters long')
