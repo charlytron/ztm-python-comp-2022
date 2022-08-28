2716d921fd37576daeebb60f38fae7b11c873149
charlytron <ctompkins32@gmail.com>
2022-08-28 09:00:09 -0500

    type conversion exercise

---
+++ section-2-introduction/type_conv_exercise.py
@@ -0,0 +1,14 @@
+# name = 'Billy'
+# age = 18
+# relationship_status = 'single'
+
+# relationship_status = 'it\'s_complicated'
+
+# print(relationship_status)
+
+# We'll create a program that guesses the user's age.
+
+birth_year = input('What year were ya born? ')
+age = 2022 - int(birth_year)
+print(f'you are {age} years old')
+
