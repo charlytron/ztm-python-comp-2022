831ff01b549ada679530ba296465009e046897b9
charlytron <ctompkins32@gmail.com>
2022-08-28 09:40:36 -0500

    changed variable name

--- section-2-introduction/password_chkr.py
+++ section-2-introduction/password_chkr.py
@@ -5,7 +5,7 @@
 username = input("What is your Username? ")
 password = input("What is your password? ")
 password_length = len(password)
-password = password_length * '*'
+hidden_password = password_length * '*'
 
 
-print(f'Hey, {username}, youse password {password} is {password_length} characters long')
+print(f'Hey, {username}, youse password {hidden_password} is {password_length} characters long')
