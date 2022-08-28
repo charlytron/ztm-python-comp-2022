7aca426644be7821d50c17e1e7e6af761abc2100
charlytron <ctompkins32@gmail.com>
2022-08-28 08:23:28 -0500

    booleans

---
+++ section-2-introduction/booleans.py
@@ -0,0 +1,16 @@
+###############################################################################
+#                            BOOLEANS                                         #
+###############################################################################
+
+# Booleans are used to store true or false values.
+
+name = 'Murgatroyd'
+is_kewl = False # False is the default value for a boolean
+
+# Murgatroyd is not a kewl person. 
+# We'll give him a jar of pomade and 
+# change the value of is_kewl to True.
+
+is_kewl = True
+
+# Booleans can control the flow of our program.
