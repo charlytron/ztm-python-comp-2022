023587bda4784130aaa51581c76a2bfcb7369d51
charlytron <ctompkins32@gmail.com>
2022-08-28 14:46:07 -0500

    lists

---
+++ section-2-introduction/lists.py
@@ -0,0 +1,18 @@
+###############################################################################
+#                               LISTS                                         #
+###############################################################################
+
+# Lists are mutable
+# Lists are ordered sequences of objects of any type
+
+li = [1, 2, 3, 4, 5]
+li2 = ['a', 'b', 'c', 'd', 'e']
+li3 = [1, 'a', 2, 'b', 3, 'c', 4, 'd', 5, 'e', True]
+
+# Remember arrays? In Python, lists are a type of array.
+
+# What is a data structure? A way to organize data into a folder or container.
+# We need the appropriate data structure to store our data.
+# Has different pros and cons of accessing data, removing data, and adding data.
+
+# Lists allow us to contain data in a specific order.
