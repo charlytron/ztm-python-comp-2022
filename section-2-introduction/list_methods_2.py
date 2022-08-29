8c0d654abab4d78c1b3a68de83bfe407276f8711
charlytron <ctompkins32@gmail.com>
2022-08-29 13:32:41 -0500

    review .index and more methods

---
+++ section-2-introduction/list_methods_2.py
@@ -0,0 +1,50 @@
+############################################################
+#                  LIST METHODS 2                          #
+############################################################
+
+basket = [1, 2, 'u', 'c', 3, 4, 5]
+print(basket.index('u')) # gives us the index of the item in the list (2)
+
+print(basket.index('u', 0, 3)) # gives us the index of the item in the list (2)
+# starting from index 0 and ending at index 3 (3)
+
+
+# python keyword
+# All words that already exist in python are keywords.
+
+# example:
+
+# def
+# del
+# from
+# import
+# is
+# in
+# return
+# class
+# continue
+# pass
+# break
+# except
+# raise
+# try
+# global
+# nonlocal
+# assert
+# True
+# False
+# ...
+
+print('u' in basket) # gives us True
+print('u' not in basket) # gives us False
+print('x' in basket) # gives us False')
+
+print('i' in 'My name is Billy') # gives us True
+
+print(basket.count(1)) # gives us 1
+print(basket.count(2)) # gives us 1
+
+
+
+
+
