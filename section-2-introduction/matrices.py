695a6bb35fa7cad9fced09898a1c5f2c07833c8d
charlytron <ctompkins32@gmail.com>
2022-08-29 08:26:22 -0500

    matrices review

---
+++ section-2-introduction/matrices.py
@@ -0,0 +1,27 @@
+###############################################################################
+#                               Matrices                                      #
+###############################################################################
+
+# A matrix is a rectangular array of numbers.
+# two dimensional array.
+# Way to describe 2d lists, i.e. multi-dimensional lists.
+
+matrix = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+# We have the main array, and the sub-arrays.
+# Why is this useful?
+
+# Matrices come up in machine learning and image processing
+# and are used to represent data that the computer can understand,
+# particularly when the data is large and complicated, with many features.
+
+matrix[0][1] # 2
+print(matrix[0][1])
+
+# We can access the second item in the first sub-array.
+
+
