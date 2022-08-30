5a6b305663d0c5db0f80a15ede83bb1f8bcfd18d
charlytron <ctompkins32@gmail.com>
2022-08-30 13:56:53 -0500

    Dictionary methods

--- section-3-python-basics/dictionary_keys.py
+++ section-3-python-basics/dictionary_keys.py
@@ -24,13 +24,33 @@
 
 # 99% of the time, a key is usually a string, something descriptive like 'name', 'age', 'height', etc.
 
-dictionary = {
-  '123': [1,2,3],
-  '123': 'HOWL', # this will overwrite the previous value
-}
+# dictionary = {
+#   '123': [1,2,3],
+#   '123': 'HOWL', # this will overwrite the previous value
+# }
 
-print(dictionary['123']) # gives us 'HOWL', but why?
+# print(dictionary['123']) # gives us 'HOWL', but why?
 # There can only be one key with the same value.
 # It represents a bookshelf in that memory location.
 
+# dictionary = {
+#   'basket': [1,2,3], # key is 'basket'
+#   'Greet': 'HOWL', # this will overwrite the previous value
+# }
 
+# print(dictionary['basket']) # gives us [1,2,3]
+
+user = {
+  'basket': [1,2,3], # key is 'basket'
+  'Greet': 'HOWL', # this will overwrite the previous value
+  'age': 25
+}
+
+print(user.get('age', 55)) # gives us 55
+# grab the age from the user dictionary, 
+# if it's not there, give us default value of 55
+# If the user already has an age, give us that age.
+
+# A less common way to create dictionaries is to use the dict() function:
+user2 = dict(name='SpillyWilly', age=25)
+print(user2) # gives us {'name': 'SpillyWilly', 'age': 25}
