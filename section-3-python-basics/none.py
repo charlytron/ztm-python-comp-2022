bab0c9d865ace4c16ce9d724b90d149f40fd9ccd
charlytron <ctompkins32@gmail.com>
2022-08-30 08:26:55 -0500

    the concept of None

---
+++ section-3-python-basics/none.py
@@ -0,0 +1,14 @@
+###############################################################################
+#                                 NONE                                        #
+###############################################################################
+
+# Some methods that work on lists return None, but they modify the list in place.
+
+# None is a special data type that represents "no value", like NULL in C or in Java.
+
+a = None
+print(a) # gives us None
+
+# Default weapons in a game are None.
+weapons = None
+print(weapons) # gives us None
