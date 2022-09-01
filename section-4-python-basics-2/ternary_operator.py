bd1823ce0c3260d191623d75f048924846d7ef23
charlytron <ctompkins32@gmail.com>
2022-09-01 08:42:54 -0500

    Ternary operator

---
+++ section-4-python-basics-2/ternary_operator.py
@@ -0,0 +1,17 @@
+############################################################
+#                      Ternary Operator                    #
+############################################################
+
+""" 
+The ternary operator is a shortcut for the if-else statement.
+It can also be called a conditional expression. 
+It's an expression because it returns a value.
+
+"""
+
+# condition_if_true if condition else condition_if_else
+
+is_bububuddy = True
+can_message = "message_allowed" if is_bububuddy else "message_not_allowed"
+
+print(can_message) # message_allowed
