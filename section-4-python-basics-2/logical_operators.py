45abc9e219d14edf572de9532f0d518d16b388d3
charlytron <ctompkins32@gmail.com>
2022-09-01 12:22:30 -0500

    Logical operators

---
+++ section-4-python-basics-2/logical_operators.py
@@ -0,0 +1,18 @@
+###############################################################################
+#                            Logical Operators                                #
+###############################################################################
+
+# Logical operators are used to combine conditional statements.
+
+if True and True:
+    print("True and True")
+
+if True or True:
+    print("True or True")
+
+print(4 > 3) # True
+print(4 == 3) # False printing 4 = 5 is not allowed, = is an assignment operator
+print(4 != 3) # True
+print(4 >= 3) # True
+print(4 <= 3) # False
+print(4 < 3) # Fal
--- section-4-python-basics-2/short_circuiting.py
+++ section-4-python-basics-2/short_circuiting.py
@@ -23,3 +23,25 @@ if is_Friend or is_User:
 # condition is met. If the first condition is already false, the second condition
 # is not even evaluated. The interpreter has already done enough work.
 
+# Why is a greater than b
+print('a' > 'b') # False
+print('a' > 'a') # False
+print('a' > 'A') # why True? 
+# ascii value of a is 97
+# ascii value of A is 65
+# The interpreter is comparing the ASCII values of the characters.
+
+print(1 < 2 < 3) # True
+print(1 < 2 > 3 < 4) # False, will short circuit
+print(1 >= 2 > 3 < 4 > 5) # False, will short circuit
+print(1 >= 0) # True
+print(0 != 0) # False
+
+# What about the not operator?
+print(not(True)) # False
+# it's a keyword but also a function
+print(not False) # True
+print(not 0) # True
+print(not 1) # False
+print(not -1) # False
+print(not(1 == 1)) # False
