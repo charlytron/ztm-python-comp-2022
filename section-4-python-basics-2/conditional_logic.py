da67ccf487edda7d7571639c79da85510a6ed2ab
charlytron <ctompkins32@gmail.com>
2022-09-01 07:54:30 -0500

    Indentation in Python

--- section-4-python-basics-2/conditional_logic.py
+++ section-4-python-basics-2/conditional_logic.py
@@ -6,17 +6,28 @@ is_licensed = True
 
 # print('check 1') # outside of the if statement
 
-# The interpreter will continue to execute 
-# the code after the if statement if 
-# the condition is not met
+"""
+ The interpreter will continue to execute
+ the code after the if statement if
+ the condition is not met
+
+"""
 
 if is_old and is_licensed:
     print("You are old enough and licensed to drive")
 # elif is_licensed:
     # print("You are licensed to drive")
 else:
-   print('checkity check check, checkmate') # outside of the if statement
+    print('checkity check check, checkmate')  # outside of the if statement
 
-print('wubba lubba dub dub') # outside of the if statement
-# We're now controlling the flow of the program.
+print('wubba lubba dub dub')  # outside of the if statement
 
+"""
+
+  We're now controlling the flow of the program.
+ If this were indented, the interpreter would
+ continue to execute the code after the else statement
+ but not the if statement.
+
+"""
+ 
