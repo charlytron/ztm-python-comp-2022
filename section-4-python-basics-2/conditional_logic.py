51585cfaabe049d2d94d7f3afc9429249786de8b
charlytron <ctompkins32@gmail.com>
2022-09-01 07:30:44 -0500

    Conditional logic review

---
+++ section-4-python-basics-2/conditional_logic.py
@@ -0,0 +1,22 @@
+is_old = False
+is_licensed = True
+
+# if is_old and is_licensed:
+#     print("You are old enough to drive")
+
+# print('check 1') # outside of the if statement
+
+# The interpreter will continue to execute 
+# the code after the if statement if 
+# the condition is not met
+
+if is_old and is_licensed:
+    print("You are old enough and licensed to drive")
+# elif is_licensed:
+    # print("You are licensed to drive")
+else:
+   print('checkity check check, checkmate') # outside of the if statement
+
+print('wubba lubba dub dub') # outside of the if statement
+# We're now controlling the flow of the program.
+
