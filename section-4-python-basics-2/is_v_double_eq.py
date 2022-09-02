c751222b39fa0a436355c96b5d23f04137c7bc6a
charlytron <ctompkins32@gmail.com>
2022-09-02 06:10:32 -0500

    'is' versus '==' continued

--- section-4-python-basics-2/is_v_double_eq.py
+++ section-4-python-basics-2/is_v_double_eq.py
@@ -8,6 +8,7 @@
 print(1 is 1) # True
 print(1 is not 1) # False
 print(1 == 1) # True
+print('1' == 1) # False (doesn't get converted to int)
 print(1 != 1) # False
 print(1 == 2) # False
 print(1 != 2) # True
@@ -23,5 +24,26 @@ print("" == 1) # False because '' is falsy
 print([] == 1) # False because [] is falsy
 print(10 == 10.0) # True
 print([] == []) # True
+print([1,2,3] == [1,2,3]) # True
+
+# == checks for value equality
+
+print(True is 1) # False
+print("" is 1) # False
+print([] is 1) # False
+print(10 is 10.0) # False
+print([] is []) # False
+print([1,2,3] is [1,2,3]) # False
+
+# is checks if the location in memory is the same
+# Things may appear to be the same but they are not the same object in memory
+# therefore is will return False
+
+a = [1,2,3]
+b = [1,2,3]
+
+a is b # False
+a == b # True because the values are the same
+
 
 # to be continued from 2:39:00 in the video
