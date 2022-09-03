5e4156862d3fdcbe6d4d7f641787888690f7f91a
charlytron <ctompkins32@gmail.com>
2022-09-03 08:59:55 -0500

    Parameters and arguments

---
+++ section-4-python-basics-2/args_v_parameters.py
@@ -0,0 +1,21 @@
+##############################################################################
+#                      Arguments vs Parameters in Python                     #
+##############################################################################
+
+# Arguments are the values passed to a function when it is called.
+# Parameters are the variables listed inside the parentheses in the function
+
+fname = "Funky"
+lname = "Winkerbean"
+
+def my_function(fname, lname): # fname and lname are parameters
+    print(f'Jello, {fname} {lname}')
+
+my_function("Gomer", "Pyle") # fname and lname are arguments
+# These arguments override the parameters
+
+# Arguments are used to pass values to a function
+
+# Parameters are used to define the values that will be passed to the function
+# Arguments are used to pass values to a function, dig?
+
