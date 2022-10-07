a8c6039e4694e5c68d12282e44427a6b4be177a2
charlytron <ctompkins32@gmail.com>
2022-10-07 07:50:50 -0500

    Writing unit tests II

--- section-15-testing-in-python/test.py
+++ section-15-testing-in-python/test.py
@@ -1,15 +1,28 @@
 import unittest
 import main
 
+
 class TestMain(unittest.TestCase):
-  def test_bar_foo(self):
-    test_param = 10
-    result = main.bar_foo(test_param)
-    self.assertEqual(result, 15)
-    
-  def test_bar_foo2(self):
-    test_param = 'oogabooga'
-    result = main.bar_foo(test_param)
-    self.assertTrue(result, ValueError)
-    
-unittest.main()
+    def test_bar_foo(self):
+        test_param = 10
+        result = main.bar_foo(test_param)
+        self.assertEqual(result, 15)
+
+    def test_bar_foo2(self):
+        test_param = 'oogabooga'
+        result = main.bar_foo(test_param)
+        self.assertIsInstance(result, ValueError)
+
+    def test_bar_foo3(self):
+        test_param = None
+        result = main.bar_foo(test_param)
+        self.assertEqual(result, 'please enter number, yo')
+
+    def test_bar_foo4(self):
+        test_param = ''
+        result = main.bar_foo(test_param)
+        self.assertEqual(result, "please enter number, yo")
+
+
+if __name__ == '__main__':
+    unittest.main()
