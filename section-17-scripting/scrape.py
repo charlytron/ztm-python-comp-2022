4b7efd6895529e1b518cd887d60577878adda63e
charlytron <ctompkins32@gmail.com>
2022-10-20 07:35:24 -0500

    Hacker News Project

--- section-17-scripting/scrape.py
+++ section-17-scripting/scrape.py
@@ -10,4 +10,15 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.find_all('div'))
+links = soup.select('.titleline > a')
+votes = soup.select('.score')
+
+def create_custom_hn(links, votes):
+  hn = []
+  for idx, item in enumerate(links):
+    title = links[idx].getText()
+    href = links[idx].get('href', None)
+    hn.append(href)
+  return hn
+
+print(create_custom_hn(links,votes))
