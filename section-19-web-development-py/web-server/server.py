abb5425ac9bc0a3fd361eccc629c66fa83842694
charlytron <ctompkins32@gmail.com>
2022-10-23 09:07:36 -0500

    Building Flask server

---
+++ section-19-web-development-py/web-server/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('./index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('./about.html')
+  
+# @app.route("/favicon.ico")
+# def blog():
+#     return "<p>Blog</p>"
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "<p>This is my dog, dog</p>"
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return render_template('./index.html')
+  
+# @app.route("/blog")
+# def blog():
+#     return "<p>Blog</p>"
+
+# @app.route("/blog")
+# def blog2():
+#     return "<p>This is my dog, dog</p>"
