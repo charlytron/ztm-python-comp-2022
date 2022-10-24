518804d9f905736fc203cb7781a07c26b4f74fd0
charlytron <ctompkins32@gmail.com>
2022-10-24 08:47:42 -0500

    URL parameters

--- section-19-web-development-py/web-server/server.py
+++ section-19-web-development-py/web-server/server.py
@@ -3,35 +3,15 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/")
-def hello_world():
-    print(url_for('static', filename='global.ico'))
-    return render_template('./index.html')
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id=post_id)
 
 @app.route("/about.html")
 def about():
     return render_template('./about.html')
   
-# @app.route("/favicon.ico")
-# def blog():
-#     return "<p>Blog</p>"
 
 @app.route("/blog/2020/dogs")
 def blog2():
     return "<p>This is my dog, dog</p>"
-
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#     return render_template('./index.html')
-  
-# @app.route("/blog")
-# def blog():
-#     return "<p>Blog</p>"
-
-# @app.route("/blog")
-# def blog2():
-#     return "<p>This is my dog, dog</p>"
