8cd861090c18c87ac374b090e721fd90f8ae9b39
charlytron <ctompkins32@gmail.com>
2022-10-24 07:59:07 -0500

    templating engine at work

--- section-19-web-development-py/web-server/server.py
+++ section-19-web-development-py/web-server/server.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+print(__name__)
 
 @app.route("/")
 def hello_world():
+    print(url_for('static', filename='global.ico'))
     return render_template('./index.html')
 
 @app.route("/about.html")
