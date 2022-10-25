1a5c392a374af119437d43221820bc69b61576c2
charlytron <ctompkins32@gmail.com>
2022-10-25 08:05:18 -0500

    Building Portfolio website I

--- section-19-web-development-py/web-server/server.py
+++ section-19-web-development-py/web-server/server.py
@@ -3,15 +3,23 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-    return render_template('./index.html', name=username, post_id=post_id)
+@app.route('/index.html')
+def my_home():
+    return render_template('./index.html')
+
+@app.route('/works.html')
+def my_works():
+    return render_template('./works.html')
 
 @app.route("/about.html")
 def about():
     return render_template('./about.html')
+
+@app.route("/contact.html")
+def contact():
+    return render_template('./contact.html')
   
 
-@app.route("/blog/2020/dogs")
-def blog2():
-    return "<p>This is my dog, dog</p>"
+@app.route("/components.html")
+def components():
+    return render_template('./components.html')
