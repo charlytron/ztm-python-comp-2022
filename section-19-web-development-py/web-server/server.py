9ac321c2d4f3eed024fb30a60a06bd6f89c050a9
charlytron <ctompkins32@gmail.com>
2022-10-25 08:17:10 -0500

    styled components

--- section-19-web-development-py/web-server/server.py
+++ section-19-web-development-py/web-server/server.py
@@ -3,22 +3,31 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
+
 @app.route('/index.html')
 def my_home():
     return render_template('./index.html')
+
 
 @app.route('/works.html')
 def my_works():
     return render_template('./works.html')
 
+
+@app.route('/work.html')
+def my_work():
+    return render_template('./work.html')
+
+
 @app.route("/about.html")
 def about():
     return render_template('./about.html')
 
+
 @app.route("/contact.html")
 def contact():
     return render_template('./contact.html')
-  
+
 
 @app.route("/components.html")
 def components():
