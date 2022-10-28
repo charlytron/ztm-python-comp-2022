14b960f7f0c07b834addcd65c5ef5ce9ace466c0
charlytron <ctompkins32@gmail.com>
2022-10-28 08:52:02 -0500

    Finished portflio, uploaded to server

--- section-19-web-development-py/web-server/server.py
+++ section-19-web-development-py/web-server/server.py
@@ -1,4 +1,6 @@
+from lib2to3.pgen2.token import NEWLINE
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -13,18 +15,19 @@ def html_page(page_name):
     return render_template(page_name)
   
   
-def write_to_file(data):
-    with open('database.txt', mode='a') as database: # a = append
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2: # a = append
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        file = database.write(f'\n{email},{subject},{message}')
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
   
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thank_you.html')
     else: 
         return 'something went wrong. Try again!'
